6bd1da4cd535ee35da69c3b20d11423ba82565c7
Jacob Levernier <jleverni@uoregon.edu>
2015-01-04 15:12:40 -0800

    Cleaning up code. Also re-added (but not yet re-tested) code for binary association matrix.

--- Code_Scratchpad.R
+++ Code_Scratchpad.R
@@ -1,61 +1,3 @@
-library(qgraph)
-
-# This is following (loosely) http://www.r-bloggers.com/qgraph-version-1-1-0-and-how-to-simply-make-a-gui-using-rpanel/
-
-data(big5)
-data(big5groups)
-qgraph(cor(big5),groups=big5groups,gui=TRUE)
-
-
-
-Edges <- data.frame(
-    from = rep(1:5,each=5),
-		    to = rep(1:5,times=5),
-				    thickness = abs(rnorm(25)))
-
-Edges <- subset(Edges,from!=to)
-
-qgraph(Edges,esize=5,gray=TRUE)
-
-# Following http://stackoverflow.com/a/20255453
-
-node.data <- data.frame(
-    one=c("This is a test","And another test","Yet a third test"),
-    two=c("And another test","Yet a third test","This is a test")
-  )
-
-# Testing hard-wrapping lines, following http://stat.ethz.ch/R-manual/R-patched/library/base/html/strwrap.html
-
-x <- "This General Public License does not permit incorporating your program into proprietary programs."
-
-# THIS WORKS FOR ARBITRARILY RESHAPING TEXT INTO BLOCKS, WITH \n SEPARATORS. This follows http://jeromyanglim.tumblr.com/post/33554853812/how-to-automatically-break-a-caption-over-multiple
-node.data_hard_wrapped <- lapply(node.data[['one']], 
-	function(x){
-		paste(
-			strwrap(x, width=5, simplify=TRUE)
-			,
-			collapse = "\n"
-			)
-		}
-)
-
-
-
-
-
-library(igraph)
-
-g <- graph.data.frame(
-  node.data_hard_wrapped,
-  directed=FALSE # make this true if your network has a direction in the links
-)
-plot(g)
-
-
-
-
-
-
 setwd("~/Desktop/Note-Taking_Network_Grapher/")
 
 data_file_to_start <- "./done2.txt"
@@ -87,10 +29,10 @@ View(node_text_dataframe)
 str(node_text_dataframe)
 
 
-rm(dataframe_to_use)
-dataframe_to_use <- data.frame(
-	text=as.Date(character()), # Just create an empt dataframe for now, following http://stackoverflow.com/a/10689206
-	tag=character(), 
+rm(edge_list)
+edge_list <- data.frame(
+	Source=as.Date(character()), # Just create an empt dataframe for now, following http://stackoverflow.com/a/10689206
+	Target=character(), 
 	stringsAsFactors=FALSE
 ) 
 
@@ -101,56 +43,26 @@ for(i in 1:length(node_text_dataframe[["hard_wrapped"]])){
 		tag <- master_tag_list[[j]]
 		if(grepl(tag, text)){
 			row_to_append <- cbind(text, tag)
-			dataframe_to_use <- rbind(dataframe_to_use, row_to_append)
-			"Yes"
-			#dataframe_to_use[[master_tag_list[i]]] <- 
-			#grepl(master_tag_list[[i]], file1.text)*1 # Following http://r.789695.n4.nabble.com/Changing-a-logical-matrix-into-a-numeric-matrix-td3206797.html, multiplying by 1 here turns a logical vector (e.g., 'TRUE', 'TRUE', 'FALSE', etc.) into a numerical one (e.g., 1, 1, 0, etc.)
-			#grep(master_tag_list[[i]], file1.text)
+			edge_list <- rbind(edge_list, row_to_append)
 		}
+		
+		binary_association_matrix[[master_tag_list[i]]] <- 
+			grepl(master_tag_list[[i]], file1.text)*1 # Following http://r.789695.n4.nabble.com/Changing-a-logical-matrix-into-a-numeric-matrix-td3206797.html, multiplying by 1 here turns a logical vector (e.g., 'TRUE', 'TRUE', 'FALSE', etc.) into a numerical one (e.g., 1, 1, 0, etc.)
 	}
 }
 
-View(dataframe_to_use)
+View(edge_list)
+View(binary_association_matrix)
 
-#library('igraph')
-#install.packages('InteractiveIGraph')
-#library('InteractiveIGraph')
+write.csv(edge_list, file="Edge_List.csv", row.names=FALSE, eol="\n", quote=TRUE)
+write.csv(binary_association_matrix, file="Binary_Association_Matrix.csv", row.names=FALSE, eol="\n", quote=TRUE)
 
-# Loosely using the tutorial at http://duomenuanalize.lt/sna-interactive-graphs-working-directly-r-graphic-device
-#gOrg <- graph.famous("Heawood")
-#plot(gOrg)
-#X11(type="Xlib") # From http://stackoverflow.com/a/16673763, for Linux support.
-#g = InteractiveIGraph(gOrg)
-# THE ABOVE (FROM INTERACTIVEIGRAPH) ISN'T VERY COMPELLING
-
-# Following http://www.r-bloggers.com/simple-network-diagrams-in-r/
-#library(qgraph) 
-# NOT WORKING:
-# qgraph(dataframe_to_use, groups = factor(colnames(dataframe_to_use)[-1]))
-
-str(dataframe_to_use)
-
-write.csv(dataframe_to_use, file="Edgelist.csv", row.names=FALSE, eol="\n", quote=TRUE)
-
-
-
-# Edges <- data.frame(
-# 	from = rep(1:5,each=5),
-# 	to = rep(1:5,times=5)
-# 	#,
-# 	#thickness = abs(rnorm(25))
-# 	)
-# 
-# Edges <- subset(Edges,from!=to)
-# 
-# library('qgraph')
-# qgraph(Edges,esize=5,gray=TRUE)
-
+# Make a network graph using the Edge List:
 
 library('qgraph')
 
 graph <- qgraph(
-	dataframe_to_use,
+	edge_list,
 	esize=5,
 	gray=TRUE,
 	label.scale=TRUE,
