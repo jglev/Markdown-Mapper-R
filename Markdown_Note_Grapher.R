0fde6cdfebd50dda1e85b6be123273756651e0f4
Jacob Levernier <jleverni@uoregon.edu>
2015-02-02 21:02:05 -0800

    Fixed regexp.

--- Markdown_Note_Grapher.R
+++ Markdown_Note_Grapher.R
@@ -128,9 +128,11 @@ for(data_file_to_start in args){
 	
 	# Perform the grep, returning all values (one vector per row):
 	# Note: This regex allows for two types of tags: '+tag', and '+{tag phrase that includes spaces}'. The latter is included because it allows +{phrases to be tagged} (which allows easier searching across text files)
+	# We are tolower()-ing tags to make them more connectable across files (since, e.g., +Tag and +tag would otherwise be counted as two separate tags).
 	tag_list_by_row <- regmatches(
 		file.text, 
-		gregexpr('\\+\\(\\w\\|{.*}\\)*',file.text)
+		gregexpr('\\+(\\w|\\{.*\\})*',file.text)
+		#\\+\\w* # Original Regex
 	)
 	
 	# Collapse the rows into a single vector, building up over loops through files:
