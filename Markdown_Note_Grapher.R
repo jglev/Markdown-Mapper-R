97e9dcc60611c31ca26ec356b40746ccb7dd8c39
Jacob Levernier <jleverni@uoregon.edu>
2015-03-12 13:42:17 -0700

    Changed positional file argument to use stdin by default, rather than generating an error message if no filenames are given.

--- Markdown_Note_Grapher.R
+++ Markdown_Note_Grapher.R
@@ -127,8 +127,9 @@ parser$add_argument("-t", "--tag-delimiter",
 parser$add_argument(
 	"files_to_parse", # Because it lacks a '-' flag, this will be interpreted as a positional argument.
 	metavar="File to parse", # What will be displayed in the help documentation.
-	nargs='+', # Gather as many filenames as are listed into a big list, and create an error message if there isn't at least one filename given (see https://docs.python.org/2/library/argparse.html#nargs)
-	help="A list of plain-text files to parse."
+	nargs='?', # Gather as many filenames as are listed into a big list, and use the default setting below if there isn't at least one filename given (see https://docs.python.org/2/library/argparse.html#nargs)
+	help="A list of plain-text files to parse.",
+	default="stdin" # Default to stdin (i.e., input from a command-line pipe)
 ) 
 
 parser$add_argument(
